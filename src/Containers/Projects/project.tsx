3cd45adf15c1fcac1b7d0d521cc0716628e9a3c7
Shankrith S <shankrith1618+github@gmail.com>
2023-01-22 20:31:54 +0530

    basic project section done

--- src/Containers/Projects/project.tsx
+++ src/Containers/Projects/project.tsx
@@ -1,16 +1,36 @@
 import styled from "styled-components";
+import { BsGithub } from "react-icons/bs";
 export interface IProjectProps {
     name: string,
     description: string,
     techStack: string[],
     imageUrl: URL,
-    githubUrl?: URL,
+    githubUrl: URL,
 }
 const ParentContainer = styled.li`
-    display:grid;
-    grid-template-columns: repeat(12,1fr);
+  position: relative;
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(12, 1fr);
+  align-items: center;
 
-    
+`;
+const ImageContainer = styled.div`
+    grid-area: 1 / 6 / -1 / -1;  
+    max-width: 500px;          
+    overflow: hidden;
+    border-radius: 5px;
+
+`;
+
+const ContentContainer = styled.div`
+    grid-area: 1 / 1 / -1 / 7;
+    text-align: left; 
+    z-index: 10;
+    display:flex;
+    flex-direction: column;
+    gap: 1rem;
+    align-items: start;
 `;
 const TechStackContainer = styled.ul`
     list-style-type:none;
@@ -18,23 +38,37 @@ const TechStackContainer = styled.ul`
     gap:1rem;
     font-weight:300;
     color: #64ffda;
+    padding: 0;
+`;
+
+const FrostedGlassBlock = styled.div`
+    padding:1rem;
+    margin: 0rem 0rem 0rem;
+    background-color: #112240;
+    border-radius: 5px;
+    overflow: hidden;
+    position:relative;
 `;
 
 export function Project(props: IProjectProps) {
     return (
         <ParentContainer>
-            <div>
-                <img src={props.imageUrl.toString()} width="700" alt="" />
-            </div>
-            <div>
+            <ContentContainer>
                 <h3>{props.name}</h3>
-                <p>{props.description}</p>
+                <FrostedGlassBlock><p>{props.description}</p></FrostedGlassBlock>
                 <TechStackContainer>
                     {props.techStack.map((tech, index) => {
                         return (<li>{tech}</li>)
                     })}
                 </TechStackContainer>
-            </div>
+                <div>
+                    <a href={props.githubUrl.toString()}><BsGithub size="24" color="white" /></a>
+                </div>
+            </ContentContainer>
+            <ImageContainer>
+                <a href=""><img src={props.imageUrl.toString()} width="500" alt="" /></a>
+
+            </ImageContainer>
         </ParentContainer>
     )
 }
