26d263a946301c770db424494d6a4b159ea88236
Shankrith S <shankrith1618+github@gmail.com>
2023-01-22 21:15:44 +0530

    contact section ui fix

--- src/Containers/Contact/Index.tsx
+++ src/Containers/Contact/Index.tsx
@@ -3,8 +3,9 @@ import styled from "styled-components";
 import { PrimaryButtonLookingLink } from "../../Components/Common";
 
 const SectionContainer = styled.section`
+    margin: 0px auto;
+    max-width: 1000px;
     color: white;
-    padding: 0rem 15rem 0rem;
     text-align:center;
 `
 const Container = styled.div`
@@ -15,9 +16,14 @@ const Container = styled.div`
     padding 0rem 15rem;
   `;
 
+const Heading2 = styled.h2`
+font-size: 32px;
+position: relative;
+`;
+
 export function ContactSection() {
     return (<SectionContainer id="contact" >
-        <h1>Get in touch</h1>
+        <Heading2>Get in touch</Heading2>
         <Container>
             <p>I'm open to opportunities and would love to work on new ideas. Whether you have a question or just want to connect, I'll try my best to get back to you!</p>
             <PrimaryButtonLookingLink href="mailto:shankrith1618@gmail.com">Say Hello</PrimaryButtonLookingLink>
