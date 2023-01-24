cc86659152ba832dcd2fad09afceca06346f284d
Shankrith S <shankrith1618+github@gmail.com>
2023-01-24 15:43:11 +0530

    set global font

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import { AboutSection } from './Containers/About/Index';
 import { WorkSection } from './Containers/Work/Index';
 import { Navbar } from './Containers/Navbar/Index';
@@ -11,13 +11,19 @@ import { HeroView } from './Containers/Hero/Index';
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './theme';
 
+const GlobalStyle = createGlobalStyle`
+  body{
+    font-family: 'Open Sans', sans-serif;
+  }
+`
+
 const AppContainer = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
   gap: 5rem;
   align-items: center;
-
+  
 `;
 
 const BodyContainer = styled.main`
@@ -32,6 +38,7 @@ const BodyContainer = styled.main`
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <AppContainer>
         <Navbar />
         <BodyContainer >
